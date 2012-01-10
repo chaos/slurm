fc76349b2ddb988d020df5f0d67cd61a7243d176
Mark A. Grondona <mgrondona@llnl.gov>
2012-01-10 11:22:12 -0800

    testsuite: Add test for blocked signals
    Add test7.15 to test for any signals blocked by default in job tasks.

---
+++ testsuite/expect/test7.15.prog.c
@@ -0,0 +1,51 @@
+/*****************************************************************************\
+ *  test7.15.prog.c - Test for blocked signals
+ *****************************************************************************
+ *  Copyright (C) 2012 LLNS, Inc.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  CODE-OCEC-09-009. All rights reserved.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.schedmd.com/slurmdocs/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+#include <stdio.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+int main (int ac, char **av)
+{
+	char hostname[1024];
+	int i, rc = 0;
+	struct sigaction act;
+
+	if (gethostname (hostname, sizeof (hostname)) < 0) {
+		fprintf (stderr, "Failed to get hostname on this node\n");
+		strcpy (hostname, "Unknown");
+	}
+	for (i = 1; i < SIGRTMAX; i++) {
+		sigaction (i, NULL, &act);
+		if (act.sa_handler == SIG_DFL)
+			continue;
+		fprintf (stderr, "%s: Signal %d appears to be ignored!\n",
+			hostname, i);
+		rc = 1;
+	}
+	return (rc);
+}
