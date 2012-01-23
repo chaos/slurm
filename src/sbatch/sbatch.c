c89e73f6a2f32f3870da8ad6b1e58e6b073a7b9a
Phil Eckert <eckert2@llnl.gov>
2012-01-23 07:14:01 -0800

    Unset user's environment at start of execution if --export-file option..
    Because the --export-file option is indicating the user's environment
    is comming from a file, the environemnt, as set in "extern char **environ,
    is now unset to insure that those variables do not overwrite the ones
    imported from the file. This is consistent with the how it works when
    spawned by Moab.

--- src/sbatch/sbatch.c
+++ src/sbatch/sbatch.c
@@ -130,12 +130,20 @@ int main(int argc, char *argv[])
 		exit(error_exit);
 	}
 
+
 	if (opt.get_user_env_time < 0) {
 		/* Moab does not propage the user's resource limits, so
 		 * slurmd determines the values at the same time that it
 		 * gets the user's default environment variables. */
 		(void) _set_rlimit_env();
 	}
+
+	/*
+	 * if the environment is coming from a file, the
+	 * environment at execution startup, must be unset.
+	 */
+	if (opt.export_file != NULL)
+		env_unset_environment();
 
 	_set_prio_process_env();
 	_set_spank_env();
@@ -387,6 +395,7 @@ static int _fill_job_desc_from_opts(job_desc_msg_t *desc)
 		desc->warn_signal = opt.warn_signal;
 	if (opt.warn_time)
 		desc->warn_time = opt.warn_time;
+
 
 	desc->environment = NULL;
 	if (opt.export_file) {
