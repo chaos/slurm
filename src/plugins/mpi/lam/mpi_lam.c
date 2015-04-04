e7bc9a833b5698c614428611585be32732aff25a
Adam Moody <moody20@llnl.gov>
2015-04-03 17:53:30 -0700

    add prefork callback to all mpi plugins

--- src/plugins/mpi/lam/mpi_lam.c
+++ src/plugins/mpi/lam/mpi_lam.c
@@ -80,6 +80,13 @@ const char plugin_name[]        = "mpi LAM plugin";
 const char plugin_type[]        = "mpi/lam";
 const uint32_t plugin_version   = 100;
 
+int p_mpi_hook_slurmstepd_prefork(const slurmd_job_t *job,
+				  char ***env)
+{
+	debug("Using mpi/lam");
+	return SLURM_SUCCESS;
+}
+
 int p_mpi_hook_slurmstepd_task(const mpi_plugin_task_info_t *job,
 			       char ***env)
 {
